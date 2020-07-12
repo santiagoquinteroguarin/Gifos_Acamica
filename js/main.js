4ffbf03ae43833a3981352123904d1ee99982779
Santiago Quintero <quinterosantiago15@gmail.com>
2020-07-11 19:01:25 -0500

    creating theme dropdown, adding styles and functionality

--- js/main.js
+++ js/main.js
@@ -11,3 +11,65 @@ function changeColorBtn(e) {
     let textBtn = document.querySelector('#btn-search');
     textBtn.style.color = '#110038';
 }
+
+// API
+const apiKey = '&api_key=ebk0jnF3WxzqiudF7E9fiRMumlMantoI';
+const URL = 'http://api.giphy.com/v1/gifs/search?q=';
+const URLTrendings = 'http://api.giphy.com/v1/gifs/trending?rating=g';
+let gifsSuggestions = document.querySelectorAll('.suggestions__content_gif-giphy');
+let gifsTrendings = document.querySelectorAll('.trendings__content');
+
+const renderTrendings = async(arrayGifs, arrayElements, elementClass, nameClass) => {
+    for(let i of arrayElements.keys()){
+        let gif = document.createElement('img');
+        gif.src = arrayGifs[Math.floor(Math.random() * (99 - 1) + 1)].images.preview_webp.url;
+        gif.className = nameClass;
+        arrayElements[i].appendChild(gif);
+    }
+}
+    
+const renderSuggestions = async(arrayGifs) => {
+    for(let i of gifsSuggestions.keys()){
+        gifsSuggestions[i].src = arrayGifs[Math.floor(Math.random() * (99 - 1) + 1)].images.preview_webp.url;   
+    }
+}
+    
+const getGifDataSuggestions = async(keyword) => {
+    const answer = await fetch(`${URL}${keyword}${apiKey}&limit=100`);
+    let data = await answer.json();
+    return data.data;
+}
+
+const getGifDataTrendings = async() => {
+    const answer = await fetch(`${URLTrendings}${apiKey}&limit=100`);
+    let data = await answer.json();
+    return data.data;
+}
+
+function callKeyword() {
+    gifKeyword = document.getElementById('search').value;
+    getGifDataSuggestions(gifKeyword)
+            .then((responseGifData) => renderSuggestions(responseGifData))
+            .catch(function(error) {
+                console.error('ERROR' + error);
+            })
+}
+    
+getGifDataSuggestions('pokemon')
+    .then((responseGifData) => renderSuggestions(responseGifData))
+    .catch(function(error) {
+        console.error('ERROR' + error);
+    })
+
+getGifDataTrendings()
+    .then(responseGifData => renderTrendings(responseGifData, gifsTrendings, '.trendings__content-gif', 'trendings__content-gif'))
+    .catch(function(error) {
+        console.error('ERROR' + error);
+    })
+
+
+// dropdown
+function dropDown() {
+    var toggle = document.getElementById("dropdown-content");
+    (toggle.style.display === "none") ? toggle.style.display = "flex" : toggle.style.display = "none";
+}
